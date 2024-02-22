5b466fd575052ae98ff8e10bf160e49c996097d7
Alexandra Petrova <96407924+leksidev@users.noreply.github.com>
2024-02-22 03:43:03 +0300

    refactor: оптимизировано меню пользователя

--- keyboards/client_keyboard.py
+++ keyboards/client_keyboard.py
@@ -1,18 +1,11 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-invoice_stages = ['Опишите груз',
-                  'Укажите вес груза в кг',
-                  'Укажите габариты груза',
-                  'Укажите адрес отправителя',
-                  'Укажите адрес получателя',
-                  'Укажите способ оплаты']
-
 main_menu_buttons = ['Сформировать накладную', 'Зарегистрировать претензию', 'Связаться с менеджером']
 
-client_main__keyboard = ReplyKeyboardMarkup(keyboard=
-                                            [[KeyboardButton(text=button_name)] for button_name in main_menu_buttons],
-                                            one_time_keyboard=False,
-                                            resize_keyboard=True,
-                                            input_field_placeholder='Выберите действие: ',
-                                            is_persistent=True,
-                                            )
+client_main_keyboard = ReplyKeyboardMarkup(keyboard=
+                                           [[KeyboardButton(text=button_name)] for button_name in main_menu_buttons],
+                                           one_time_keyboard=False,
+                                           resize_keyboard=True,
+                                           input_field_placeholder='Выберите действие: ',
+                                           is_persistent=True,
+                                           )
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import os
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 
-from db.connect import create_tables
+from database import create_tables, delete_tables
 from utils.commands import set_commands
+
+from handlers import main_menu, create_invoice, create_claim
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
@@ -13,12 +15,13 @@ if os.path.exists(dotenv_path):
 
 
 async def start_bot(bot: Bot) -> None:
-    await set_commands(bot)
     await create_tables()
+    await set_commands(bot)
     await bot.send_message(os.environ["ADMIN_ID"], text="Бот запущен!")
 
 
 async def stop_bot(bot: Bot):
+    await delete_tables()
     await bot.send_message(os.environ["ADMIN_ID"], text="Бот остановлен!")
 
 
@@ -33,6 +36,8 @@ async def start():
 
     dp.startup.register(start_bot)
     dp.shutdown.register(stop_bot)
+
+    dp.include_routers(create_invoice.router, create_claim.router, main_menu.router)
 
     try:
         await dp.start_polling(bot)
